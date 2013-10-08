b3c3425c5876cefbed7d323a415cc52ec7c43a1e
Felix Ebert <felix.ebert@if-core.de>
2013-10-08 23:29:42 +0200

    refactoring

--- converter/converter.js
+++ converter/converter.js
@@ -8,38 +8,44 @@ var input = JSON.parse(fs.readFileSync(argv._[0], {
 	encoding: 'UTF-8'
 }));
 
-var cropAndResize = function(img, currentSize, imgPath) {
-	var cropX = img.cropAreaX1 / 100 * currentSize.width;
-	var cropY = img.cropAreaY1 / 100 * currentSize.height;
-	var cropWidth = (img.cropAreaX2 / 100 * currentSize.width) - cropX;
-	var cropHeight = (img.cropAreaY2 / 100 * currentSize.height) - cropY;
-	var destName = img.name && img.name.length > 0 ? img.name + '.jpg' : img.source;
+var Image = function(def, size, imgPath, targetSizes, outputPath) {
+	var cropX = def.cropAreaX1 / 100 * size.width;
+	var cropY = def.cropAreaY1 / 100 * size.height;
+	var cropWidth = (def.cropAreaX2 / 100 * size.width) - cropX;
+	var cropHeight = (def.cropAreaY2 / 100 * size.height) - cropY;
+	var destName = def.name && def.name.length > 0 ? def.name + '.jpg' : def.source;
 
-	var imgToSmall = false;
-	if (!argv.i) {
-		input.targetSizes.forEach(function(targetSize) {
+	var isTooSmall = function() {
+		var tooSmall = false;
+		targetSizes.forEach(function(targetSize) {
 			if (targetSize.width > cropWidth) {
-				imgToSmall = true;
+				tooSmall = true;
 			}
 		});
-	}
+		return tooSmall;
+	};
 
-	if (imgToSmall) {
-		console.warn(destName + ' is too small');
-	} else {
-		input.targetSizes.forEach(function(targetSize) {
-			var dest = outputPath + path.sep + targetSize.name + path.sep + destName;
+	var cropAndResizeTo = function(targetSize) {
+		var dest = outputPath + path.sep + targetSize.name + path.sep + destName;
 
-			gm(imgPath).profile('sRGB.icc').crop(cropWidth, cropHeight, cropX, cropY).resize(targetSize.width).quality(80).noProfile().write(dest,
-					function(err) {
-						if (!err) {
-							console.log(dest + ' written');
-						} else {
-							console.error('error converting ' + destName);
-						}
-					});
+		gm(imgPath).profile('sRGB.icc').crop(cropWidth, cropHeight, cropX, cropY).resize(targetSize.width).quality(80).noProfile().write(dest, function(err) {
+			if (!err) {
+				console.log(dest + ' written');
+			} else {
+				console.error('error converting ' + destName);
+			}
 		});
-	}
+	};
+
+	this.cropAndResize = function() {
+		if (!argv.i && isTooSmall(cropWidth)) {
+			console.warn(destName + ' is too small');
+		} else {
+			targetSizes.forEach(function(targetSize) {
+				cropAndResizeTo(targetSize);
+			});
+		}
+	};
 };
 
 try {
@@ -52,12 +58,13 @@ try {
 	}
 }
 
-input.images.forEach(function(img) {
-	var imgPath = inputPath + path.sep + img.source;
+input.images.forEach(function(imgDef) {
+	var imgPath = inputPath + path.sep + imgDef.source;
 
-	gm(imgPath).size(function(err, value) {
+	gm(imgPath).size(function(err, imgSize) {
 		if (!err) {
-			cropAndResize(img, value, imgPath);
+			var image = new Image(imgDef, imgSize, imgPath, input.targetSizes, outputPath);
+			image.cropAndResize();
 		}
 	});
 });
